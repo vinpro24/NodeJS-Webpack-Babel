c48da82f775f0be8be54ae8917b929e923ffd21b
VinPro <>
2020-01-17 00:35:53 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-import http from 'http'
+import 'dotenv/config';
 import express from 'express'
-import socketIO from 'socket.io'
 
-import config from './config'
-import middleware from './services/middleware'
-// import database from './services/database'
-import apiRoutes from './routes/apiRoutes'
-// import { socketConnection } from './modules/socketio/socketio.controller'
-
-const app = express()
-const server = http.Server(app)
-
-app.io = socketIO(server)
-
-server.listen(config.PORT, () => {
-    console.log(`Server started on port ${config.PORT}`)
-    // database()
-    middleware(app)
-    apiRoutes(app)
-    // socketConnection(app.io)
-})
+console.log('Hello Node.js project.');
+console.log(process.env.MY_SECRET);
